c904613d1137f6bf3a37d2865bc680dbeab09ce9
JesusGuirao <jesguime@gmail.com>
2020-11-26 14:35:53 +0100

    Update NewMain.java

--- laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
+++ laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
@@ -17,6 +17,7 @@ public class NewMain {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("cua");
+        System.out.println("soyjesus");
     }
     
 }
