295fcdf3d6058c24bab0ee69eb59010a41fbf782
SergioMartinezFelix <74304693+SergioMartinezFelix@users.noreply.github.com>
2020-11-30 12:32:20 +0100

    Update NewMain.java
    prueba 30 noviembre

--- laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
+++ laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
@@ -20,7 +20,9 @@ public class NewMain {
         System.out.println("soyjesus");
         System.out.println("soyCandido");
         System.out.println("Va esta puta mierda ya o qué cojones pasa");
-
+        System.out.println("lkjasfkjlfdskljdfs");
+        
+        
     }
 
 }
