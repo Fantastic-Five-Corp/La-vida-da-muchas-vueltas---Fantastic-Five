cee2bfa3dba5089dbc6f0fdebfa3ff1b8eade366
SergioMartinezFelix <74304693+SergioMartinezFelix@users.noreply.github.com>
2020-11-30 15:08:02 +0100

    Los apellidos
    Los apellidos ya van!!!

--- laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
+++ laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
@@ -19,16 +19,27 @@ public class NewMain {
     public static void main(String[] args) {
         // TODO code application logic here
     
+        String[] apellidosyo=NewMain.apellidoAleatorio();
+        System.out.println(apellidosyo);
         
         //probando si los comentarios también hacen push hola que tal todo bien DEP Maradona
         // a mí me va bien se actualizan los cambios de netbeans y tal 
-        
-        
-        
+     
       
 }
-public static String apellidoAleatorio(String texto){
+public static String[] apellidoAleatorio(){       
             java.util.Random aleatorio = new java.util.Random();
             String apellido[]={"Gómez","Martínez","Rodríguez"};              
             System.out.println(" "+apellido[aleatorio.nextInt(apellido.length)]);
+
+            
+            
+            
+            return apellido; 
 }
+
+
+
+}
+
+
