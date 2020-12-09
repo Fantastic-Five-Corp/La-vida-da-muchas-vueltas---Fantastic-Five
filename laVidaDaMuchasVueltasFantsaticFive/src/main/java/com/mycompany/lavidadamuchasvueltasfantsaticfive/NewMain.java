3875825b37c7ef65024d753f2728a642c0a79658
SergioMartinezFelix <74304693+SergioMartinezFelix@users.noreply.github.com>
2020-12-09 19:33:56 +0100

    Revert "Merge branch 'main' of https://github.com/Fantastic-Five-Corp/La-vida-da-muchas-vueltas---Fantastic-Five into main"
    This reverts commit d4ccf4d4c366cbcbfe0dd4991a408e5686e9ddd8, reversing
    changes made to f33128e6aebbd3562eb5dade8ef29ded8d187e75.

--- laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
+++ laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
@@ -8,13 +8,8 @@ package com.mycompany.lavidadamuchasvueltasfantsaticfive;
 /**
  *
  * @author FantasticFive Corp
- *
- * @author Sergio Martínez Félix 
- * @author Germán Pascual 
- * @author Cándido Vidal 
- * @author Fernando Claudio Moreno 
  * 
- * @version 0.0.2
+ * @version 0.0.1
  *
  */
 public class NewMain {
@@ -25,45 +20,24 @@ public class NewMain {
     public static void main(String[] args) {
         // TODO code application logic her
         //String generoYo = FuncionesPersona.generoAleatorio();
-        boolean generoMama = false;
-        boolean generoPapa = true;
-        
-        String nombrePapa = FuncionesPersona.nombreAleatorio(generoPapa);
-        String nombreMama = FuncionesPersona.nombreAleatorio(generoMama);
-        String apellidosMama = FuncionesPersona.apellidoAleatorio() + " " + FuncionesPersona.apellidoAleatorio();
-        String apellidosPapa = FuncionesPersona.apellidoAleatorio() + " " + FuncionesPersona.apellidoAleatorio();
-        int sociabilidadPapa = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int empatiaPapa =FuncionesPersona.valorEstadisticaAleatorio(50);
-        int inteligenciaPapa = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int velocidadPapa = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int fuerzaPapa = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int sociabilidadMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int empatiaMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int inteligenciaMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int velocidadMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int fuerzaMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        
-        
-        int fuerzaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        int empatiaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        int sociabilidadMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        int inteligenciaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        int velocidadMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        
-
-        
         Boolean generoYo = FuncionesPersona.generoAleatorio();
-        System.out.println(generoYo?"mi genero es masculino":"mi genero es femenino");
+        if (generoYo = true) {
+            System.out.println("hombre");
+
+            // System.out.println(generoYo);
             String nombreYo = FuncionesPersona.nombreAleatorio(generoYo);
             System.out.println(nombreYo);                               //por lo que no necesariamente congeniaria con el nombre
             String apellidosYo = FuncionesPersona.apellidoAleatorio();
             System.out.println(apellidosYo);
+        String nombreYo = FuncionesPersona.nombreAleatorio(generoYo);
+        System.out.println(nombreYo);                               //por lo que no necesariamente congeniaria con el nombre
+        String apellidosYo = FuncionesPersona.apellidoAleatorio();
+        System.out.println(apellidosYo);
         
-           String persona = FuncionesPersona.ImprimePersona(nombreYo, apellidosYo, generoYo, sociabilidadMaxYo, empatiaMaxYo, inteligenciaMaxYo, velocidadMaxYo, fuerzaMaxYo);
-           System.out.println(persona);
-            
-           
-
+  
     }
 
         }
+
+    }
+}
