84298a0d4635d3c3824df95cb24f3d7516006dfc
vvificola <74908353+vvificola@users.noreply.github.com>
2020-12-09 19:41:52 +0100

    Arreglo 3 (Versión actualizada) NO TOCAR

--- laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/FuncionesPersona.java
+++ laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/FuncionesPersona.java
@@ -20,81 +20,93 @@ import java.util.Random;
  * 
  */
 public class FuncionesPersona {
-    
+
 public static Boolean generoAleatorio() {
-        java.util.Random aleatorio = new java.util.Random();
-        boolean genero = aleatorio.nextBoolean();
-        
-        return genero;
-    }
+ java.util.Random aleatorio = new java.util.Random();
+ boolean genero = aleatorio.nextBoolean();
 
-    public static String nombreAleatorio(Boolean genero) {
+ return genero;
+ }
+ public static String nombreAleatorio(Boolean genero) {
+ java.util.Random aleatorio = new java.util.Random();
+ String nombreSeleccionado = "";
+ if (genero==true) {
+ String nombresHombre[] = {"Juan", "Carlos", "Luís", "Fernando",
+"Sergio", "Cándido","Germán", "Jesús", "Miguel", "Diego Armando"};
+ nombreSeleccionado =
+nombresHombre[aleatorio.nextInt(nombresHombre.length)];
+ } else {
+ String nombresMujer[] = {"Ana", "María", "Manoli", "Rosalía",
+"Isabel", "Marie", "Jennifer","Teresa","Martirio", "Amelie" };
+ nombreSeleccionado =
+nombresMujer[aleatorio.nextInt(nombresMujer.length)];
+ }
+ return nombreSeleccionado;
+ }
+ public static String apellidoAleatorio() {
+ java.util.Random aleatorio = new java.util.Random();
+ String apellidos[] = {"Gómez", "Martínez", "Rodríguez", "Felix",
+"Houellebecq", "Cervantes", "Turing", "Curie", "Zambrano", "Maradona", "Curie",
+"Pantoja", "Foster-Wallace", "Steinbeck" };
+ String apellidoSeleccionado =
+apellidos[aleatorio.nextInt(apellidos.length)];
+ return apellidoSeleccionado;
+ }
 
-        java.util.Random aleatorio = new java.util.Random();
-        String nombreSeleccionado = "";
-        if (genero==true) {
-            String nombresHombre[] = {"Juan", "Carlos", "Luís", "Fernando", "Sergio", "Cándido","Germán", "Jesús", "Miguel", "Diego Armando"};
-            nombreSeleccionado = nombresHombre[aleatorio.nextInt(nombresHombre.length)];
-        } else {
-            String nombresMujer[] = {"Ana", "María", "Manoli", "Rosalía", "Isabel", "Marie", "Jennifer","Teresa","Martirio", "Amelie" };
-            nombreSeleccionado = nombresMujer[aleatorio.nextInt(nombresMujer.length)];
-        }
-        return nombreSeleccionado;
-    }
 
-    public static String apellidoAleatorio() {
-        java.util.Random aleatorio = new java.util.Random();
-        String apellidos[] = {"Gómez", "Martínez", "Rodríguez", "Felix", "Houellebecq", "Cervantes", "Turing", "Curie", "Zambrano", "Maradona", "Curie", "Pantoja", "Foster-Wallace", "Steinbeck" };
-        String apellidoSeleccionado = apellidos[aleatorio.nextInt(apellidos.length)];
+ public static byte valorEstadisticaAleatorio(){
 
-        return apellidoSeleccionado;
-    }
-    
-    
-    public static int valorEstadisticaAleatorio(){
-    
-    Random aleatorio = new Random();
-    int numeroAleatorio=aleatorio.nextInt((101-0));
-    
-    return numeroAleatorio; 
-    }
-    
-     public static int valorEstadisticaAleatorio(int min){
-   
-    Random aleatorio = new Random();
-    int numeroAleatorio=aleatorio.nextInt((101-min)+min);
-    
-    return numeroAleatorio; 
-    
-    
-    }
-     
-     public static int valorEstadisticaAleatorio(int min, int max){
-   
-    Random aleatorio = new Random();
-    int numeroAleatorio=aleatorio.nextInt(max-min)+min;
-    
-    return numeroAleatorio; 
-    } 
-     
-     public static String ImprimePersona (String nombre, String apellidos, boolean genero, int sociabilidad, int empatia, int inteligencia, int velocidad,int fuerza){
-     
-         String persona = "";
-         
-         persona += (nombre + " " + apellidos + ((genero ? "(Hombre)":"(Mujer)")) + "\n");
-         persona += "----------------\n";
-         persona += ("Sociabilidad:" + sociabilidad +"\n");
-         persona += ("empatia: " + empatia+"\n");
-         persona += ("inteligencia: " + inteligencia+"\n");
-         persona += ("velocidad: " + velocidad+"\n");
-         persona += ("fuerza: " + fuerza+"\n");
-         
-         
-       
-        
-         return persona;
-         
-         
-     }
+ return (byte) (new Random().nextInt(101));
+
+ }
+
+ public static byte valorEstadisticaAleatorio(byte min){
+ return (byte) (new Random().nextInt((101-min)+min));
+ }
+
+ public static byte valorEstadisticaAleatorio(byte min, byte max){
+ return (byte) (new Random().nextInt(max-min)+min);
+ }
+
+ public static String ImprimePersona (String nombre, String apellidos,
+boolean genero, int sociabilidad, int empatia, int inteligencia, int
+velocidad,int fuerza){
+
+ String persona = "";
+
+ persona += (nombre + " " + apellidos + " " + ((genero ?
+"(Hombre)":"(Mujer)")) + "\n");
+ persona += "----------------\n";
+ persona += ("Sociabilidad:" + sociabilidad +"\n");
+ persona += ("empatia: " + empatia+"\n");
+ persona += ("inteligencia: " + inteligencia+"\n");
+ persona += ("velocidad: " + velocidad+"\n");
+ persona += ("fuerza: " + fuerza+"\n");
+
+
+ return persona;
+ }
+
+ public static String ImprimeHermano (String nombre, String apellidos,
+boolean genero, int sociabilidad, int empatia, int inteligencia, int
+velocidad,int fuerza){
+
+ String hermano = "";
+
+ hermano += (nombre + " " + apellidos + " " + ((genero ?
+"(Hombre)":"(Mujer)")) + "\n");
+ hermano += "----------------\n";
+ hermano += ("Sociabilidad:" + sociabilidad +"\n");
+ hermano += ("empatia: " + empatia+"\n");
+ hermano += ("inteligencia: " + inteligencia+"\n");
+ hermano += ("velocidad: " + velocidad+"\n");
+ hermano+= ("fuerza: " + fuerza+"\n");
+
+
+ return hermano;
+ }
+
+
+
 }
     
--- laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
+++ laVidaDaMuchasVueltasFantsaticFive/src/main/java/com/mycompany/lavidadamuchasvueltasfantsaticfive/NewMain.java
@@ -18,49 +18,149 @@ package com.mycompany.lavidadamuchasvueltasfantsaticfive;
  *
  */
 public class NewMain {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic her
-        //String generoYo = FuncionesPersona.generoAleatorio();
-        boolean generoMama = false;
-        boolean generoPapa = true;
-        
-        String nombrePapa = FuncionesPersona.nombreAleatorio(generoPapa);
-        String nombreMama = FuncionesPersona.nombreAleatorio(generoMama);
-        String apellidosMama = FuncionesPersona.apellidoAleatorio() + " " + FuncionesPersona.apellidoAleatorio();
-        String apellidosPapa = FuncionesPersona.apellidoAleatorio() + " " + FuncionesPersona.apellidoAleatorio();
-        int sociabilidadPapa = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int empatiaPapa =FuncionesPersona.valorEstadisticaAleatorio(50);
-        int inteligenciaPapa = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int velocidadPapa = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int fuerzaPapa = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int sociabilidadMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int empatiaMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int inteligenciaMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int velocidadMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        int fuerzaMama = FuncionesPersona.valorEstadisticaAleatorio(50);
-        
-        
-        int fuerzaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        int empatiaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        int sociabilidadMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        int inteligenciaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        int velocidadMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
-        
-
-        
-        Boolean generoYo = FuncionesPersona.generoAleatorio();
-        System.out.println(generoYo?"mi genero es masculino":"mi genero es femenino");
-            String nombreYo = FuncionesPersona.nombreAleatorio(generoYo);
-            System.out.println(nombreYo);                               //por lo que no necesariamente congeniaria con el nombre
-            String apellidosYo = FuncionesPersona.apellidoAleatorio();
-            System.out.println(apellidosYo);
-        
-           String persona = FuncionesPersona.ImprimePersona(nombreYo, apellidosYo, generoYo, sociabilidadMaxYo, empatiaMaxYo, inteligenciaMaxYo, velocidadMaxYo, fuerzaMaxYo);
-           System.out.println(persona);
+ /**
+ * @param args the command line arguments
+ */
+ public static void main(String[] args) {
+ // TODO code application logic her
+ //String generoYo = FuncionesPersona.generoAleatorio();
+ boolean generoMama = false;
+ boolean generoPapa = true;
+ boolean vivoYo=true;
+ boolean vivoMama=true;
+ boolean vivoPapa=true;
 
 
-    }
+
+ String nombrePapa = FuncionesPersona.nombreAleatorio(generoPapa);
+ String nombreMama = FuncionesPersona.nombreAleatorio(generoMama);
+ String apellidosMama = FuncionesPersona.apellidoAleatorio() + " " +
+FuncionesPersona.apellidoAleatorio();
+ String apellidosPapa = FuncionesPersona.apellidoAleatorio() + " " +
+FuncionesPersona.apellidoAleatorio();
+ byte sociabilidadPapa =
+FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte empatiaPapa =FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte inteligenciaPapa =
+FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte velocidadPapa =
+FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte fuerzaPapa = FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte edadPapa = FuncionesPersona.valorEstadisticaAleatorio((byte)30,
+(byte)100);
+ byte sociabilidadMama =
+FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte empatiaMama = FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte inteligenciaMama =
+FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte velocidadMama =
+FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte fuerzaMama = FuncionesPersona.valorEstadisticaAleatorio((byte)50);
+ byte edadMama=FuncionesPersona.valorEstadisticaAleatorio((byte)30,
+(byte)100);
+
+
+ byte fuerzaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
+ byte empatiaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
+ byte sociabilidadMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
+ byte inteligenciaMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
+ byte velocidadMaxYo = FuncionesPersona.valorEstadisticaAleatorio();
+ byte sociabilidadYo =FuncionesPersona.valorEstadisticaAleatorio((byte)0,
+(byte)5);
+ byte empatiaYo =FuncionesPersona.valorEstadisticaAleatorio((byte)0,
+(byte)5);
+ byte inteligenciaYo =FuncionesPersona.valorEstadisticaAleatorio((byte)0,
+(byte)5);
+ byte velocidadYo =FuncionesPersona.valorEstadisticaAleatorio((byte)0,
+(byte)5);
+ byte fuerzaYo =FuncionesPersona.valorEstadisticaAleatorio((byte)0,
+(byte)5);
+
+ byte edadYo=0;
+
+
+ 
+
+ java.util.Random aleatorio =new java.util.Random ();
+ byte numHermanos = (byte)((byte) aleatorio.nextInt(4)+0);
+
+
+ for (numHermanos=0; numHermanos <3; numHermanos++){
+
+
+ if (numHermanos==1){
+ byte fuerzaMaxHermano1 = FuncionesPersona.valorEstadisticaAleatorio();
+ byte empatiaMaxHermano1 = FuncionesPersona.valorEstadisticaAleatorio();
+ byte sociabilidadHermano1 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte inteligenciaHermano1 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte velocidadMaxHermano1 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte edadHermano1= FuncionesPersona.valorEstadisticaAleatorio((byte)0,
+(byte)30);
+ String apellidosHermano1 = apellidosPapa.substring(0,
+apellidosPapa.indexOf(" ")) + " " + apellidosMama.substring(0,
+apellidosMama.indexOf(" "));
+ String nombreHermano1=
+FuncionesPersona.nombreAleatorio(FuncionesPersona.generoAleatorio());
+ }
+
+ if(numHermanos==2){
+ byte fuerzaMaxHermano2 = FuncionesPersona.valorEstadisticaAleatorio();
+ byte empatiaMaxHermano2 = FuncionesPersona.valorEstadisticaAleatorio();
+ byte sociabilidadHermano2 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte inteligenciaHermano2 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte velocidadMaxHermano2 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte edadHermano2= FuncionesPersona.valorEstadisticaAleatorio((byte)0,
+(byte)30);
+ String apellidos2= apellidosPapa.substring(0, apellidosPapa.indexOf("
+")) + " " + apellidosMama.substring(0, apellidosMama.indexOf(" "));
+ String nombreHermano2=
+FuncionesPersona.nombreAleatorio(FuncionesPersona.generoAleatorio());
+
+ }
+
+ if (numHermanos==3){
+ byte fuerzaMaxHermano3 = FuncionesPersona.valorEstadisticaAleatorio();
+ byte empatiaMaxHermano3 = FuncionesPersona.valorEstadisticaAleatorio();
+ byte sociabilidadHermano3 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte inteligenciaHermano3 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte velocidadMaxHermano3 =
+FuncionesPersona.valorEstadisticaAleatorio();
+ byte edadHermano3= FuncionesPersona.valorEstadisticaAleatorio((byte)0,
+(byte)30);
+ String apellidosHermano3 = apellidosPapa.substring(0,
+apellidosPapa.indexOf(" ")) + " " + apellidosMama.substring(0,
+apellidosMama.indexOf(" "));
+ String nombreHermano3=
+FuncionesPersona.nombreAleatorio(FuncionesPersona.generoAleatorio());
+ }
+ }
+
+ 
+
+
+
+
+ Boolean generoYo = FuncionesPersona.generoAleatorio();
+
+ String nombreYo = FuncionesPersona.nombreAleatorio(generoYo);
+ String apellidosYo = apellidosPapa.substring(0, apellidosPapa.indexOf("")) + " " + apellidosMama.substring(0, apellidosMama.indexOf(" "));
+ String persona = FuncionesPersona.ImprimePersona(nombreYo, apellidosYo,
+generoYo, sociabilidadMaxYo, empatiaMaxYo, inteligenciaMaxYo, velocidadMaxYo,
+fuerzaMaxYo);
+
+
+ System.out.println(persona);
+
+
+
+
+ }
+ }
